05aecb6f6c23c87217671dcf2f0c3b6f590048d1
Maria Quinton <maria.quinton28@gmail.com>
2020-12-05 12:30:43 -0500

    Updated

---
+++ Resources/schema.sql
@@ -0,0 +1,28 @@
+CREATE TABLE mobility_per_date (
+  date DATE PRIMARY KEY,
+  retail_and_recreation_percent_change_from_baseline INT,
+  grocery_and_pharmacy_percent_change_from_baseline INT,
+  parks_percent_change_from_baseline INT,
+  transit_stations_percent_change_from_baseline INT,
+  workplaces_percent_change_from_baseline INT,
+  residential_percent_change_from_baseline INT
+);
+
+CREATE TABLE IN_Cases (
+  date DATE PRIMARY KEY,
+  State VARCHAR,
+  Population INT,
+  total_cases INT,
+  total_cases_per_100000 INT,
+  total_deaths INT,
+  new_cases INT,
+  new_deaths INT,
+  new_deaths_per_100_000 INT,
+  new_cases_per_100_000 INT
+);
+
+CREATE TABLE indiana_newcases (
+    date DATE PRIMARY KEY,
+	New_Cases INT,
+	Seven_Day_Moving_Avg INT	
+);
